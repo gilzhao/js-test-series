ece1c6766ea2e5fb88ec0742697253bc38919bd4
gil.zhao <gil.zhao@qq.com>
2020-12-17 23:07:49 +0800

    file

---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './Task';
+
+const ToDoList = (props) => {
+	return (
+		<ul>
+			{props.tasks.map((task) => (
+				<Task key={task.id} id={task.id} name={task.name} />
+			))}
+		</ul>
+	);
+};
+
+export default ToDoList;
---
+++ src/TodoList.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import ToDoList from './TodoList';
+
+describe('ToDoList component', () => {
+	describe('when provided with an empty array of tasks', () => {
+	  it('contains an empty <ul> element', () => {
+		const toDoList = shallow(<ToDoList tasks={[]} />);
+		expect(toDoList).toContainReact(<ul />);
+	  });
+  
+	  it('does not contain any <li> elements', () => {
+		const toDoList = shallow(<ToDoList tasks={[]} />);
+		expect(toDoList.find('li').length).toEqual(0);
+	  });
+	});
+  
+	describe('when provided with an array of tasks', () => {
+	  const tasks = [
+		{
+		  id: 0,
+		  name: 'Wash the dishes',
+		},
+		{
+		  id: 1,
+		  name: 'Make the bed',
+		},
+	  ];
+  
+	  it('passes them to the Task components', () => {
+		const toDoListInstance = shallow(<ToDoList tasks={tasks} />);
+  
+		toDoListInstance.find('Task').forEach((taskInstance) => {
+		  const taskProps = taskInstance.props();
+		  const matchingTask = tasks.find((task) => task.id === taskProps.id);
+		  expect(taskProps.name).toBe(matchingTask.name);
+		});
+	  });
+  
+	  it('contains a matching number of <li> elements', () => {
+		const toDoListInstance = mount(<ToDoList tasks={tasks} />);
+  
+		toDoListInstance.find('Task').forEach((taskInstance) => {
+		  const taskProps = taskInstance.props();
+		  const matchingTask = tasks.find((task) => task.id === taskProps.id);
+		  const listItem = taskInstance.first('li');
+		  expect(listItem.text()).toBe(matchingTask.name);
+		});
+	  });
+  
+	  it('should render correctly', () => {
+		const toDoListInstance = shallow(<ToDoList tasks={tasks} />);
+		expect(toDoListInstance).toMatchSnapshot();
+	  });
+	});
+  
+
+  
+
+
+  });
